620ea4c929b34c6c5df59ce2c76040cb86d4b3e0
eliassouza93 <elias99-souza@hotmail.com>
2025-03-29 16:23:53 -0300

    'tabelas'

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database.js'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'mensagem servidor'
+app.get('/', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
