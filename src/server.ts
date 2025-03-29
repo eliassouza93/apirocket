ec904239f92760327139bb93bc10f5a53b886d5e
eliassouza93 <elias99-souza@hotmail.com>
2025-03-29 14:36:20 -0300

    'estrutura-da-aplicação'

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
-
-app.get('/', (req, res) => {
-    return 'Olá, Mundo!'
-
+app.get('/', () => {
+  return 'mensagem servidor'
 })
 
-app.listen({
+app
+  .listen({
     port: 3333,
-
-}).then(() =>{
+  })
+  .then(() => {
     console.log('servidor http rodando na porta 3333')
-})
+  })
