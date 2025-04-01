04e0262c7ba667c00040c894f4f6bca15323779b
Elias de Souza <elias99-souza@hotmail.com>
2025-04-01 08:14:16 -0300

    'tipagem'

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,9 +1,16 @@
-/* eslint-disable prettier/prettier */
 // eslint-disable-next-line
-import { Knex } from 'knex'
+import { Knex } from "knex";
 
-declare module 'knex/types/tables' { }
+declare module 'knex/types/tables' {
+    export interface Tables{
+        transactions:{
+            id:string
+            title:string
+            amount:number
+            created_at:string
+            session_id?: string
 
-
-
+        }
+    }
+}
 
